9de26950842ee0abab8a99b623a5b4e016ab87fa
LiuQifeng <qifeng@icourt.cc>
2018-04-20 17:20:17 +0800

    添加注释

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+/**
+ * webpack.config.js 是运行在 node 环境的，所以可以使用任何 node 模块，如
+ * path
+ */
+
 const path = require('path')
-const UglifyPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
+  // 指定入口文件
   entry: './src/index.js',
+  // 指定编辑后的出口文件
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
